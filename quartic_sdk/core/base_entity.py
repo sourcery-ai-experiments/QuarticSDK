6d1ad2004df43d518188d8e59bf4d92ea8c54a5c
mprasoonquartic <mayank@quartic.ai>
2021-02-09 19:55:19 +0000

    Add iterator

--- quartic_sdk/core/base_entity.py
+++ quartic_sdk/core/base_entity.py
@@ -22,30 +22,67 @@ class BaseEntity:
     of specific types
     """
 
-    def __init__(self, body_json, api_helper):
+    def __str__(self):
         """
+        Override the method to return the asset name with id
+        """
+        return f"{self.class_type}: {self.name}_{self.id}" if hasattr(self, "name") else f"{self.class_type}: {self.id}"
+
+    def __init__(self, body_json, class_type, api_helper):
+        """
+        Initialize the given entity. As a part of the initialization, we set the keys
+        of `body_json` as the attributes of the class
+        :param body_json: Dict containing the body of the entity
+        :param api_helper: APIHelper class instance containing configuration details
         """
         for key in body_json:
             setattr(self, key, body_json[key])
         self.api_helper = api_helper
+        self.class_type = class_type
 
     def get(self, name):
         """
+        Return the value of the given `name` attribute
+        :param name: (string) The attribute name to be returned
+        :return: The returned attribute value
         """
         return getattr(self, name)
 
+    def __eq__(self, other):
+        """
+        Override equals to check equality of all attributes
+        """
+        assert len(self.__dict__) == len(other.__dict__)
+        for key in self.__dict__:
+            if self.__dict__[key] != other.__dict__[key]:
+                return False
+        return True
+
 
 class AssetEntity(BaseEntity):
+    """
+    The given class refers to the asset entity which is created based upon the
+    asset object returned from the API
+    """
 
     def tags(self):
+        """
+        The given method returns the list of tags for the given asset
+        """
         tags_response = self.api_helper.call_api(
             GET_TAGS, "GET", self.id).json()
         return EntityFactory("Tag", tags_response, self.api_helper)
 
     def event_frames(self):
+        """
+        The given method returns the list of event frames for the given asset
+        """
         raise NotImplementedError
 
     def batches(self):
+        """
+        The given method returns the list of batches for the given asset
+        """
         batches_response = self.api_helper.call_api(
             GET_BATCHES, "GET", self.id).json()
         return EntityFactory("Batch", batches_response, self.api_helper)
@@ -55,22 +92,62 @@ class AssetEntity(BaseEntity):
 
 
 class TagEntity(BaseEntity):
+    """
+    The given class refers to the tag entity which is created based upon the
+    tag object returned from the API
+    """
+
+    def __str__(self):
+        """
+        Override the method to return the tag name with id
+        """
+        return f"Tag: {self.name}_{self.id}"
 
     def data(self):
         pass
 
 
 class ContextFrameEntity(BaseEntity):
+    """
+    The given class refers to the context frame entity which is created based
+    upon the context frame object returned from the API
+    """
+
+    def __str__(self):
+        """
+        Override the method to return the context frame id
+        """
+        return f"ContextFrame: {self.id}"
 
     def occurrences(self):
+        """
+        Return the list of occurrences for the given context frame
+        """
         occurrences_response = self.api_helper.call_api(
             GET_CONTEXT_FRAME_OCCURRENCES, "GET", self.id).json()
         return EntityFactory("ContextFrameOccurrence", occurrences_response, self.api_helper)
 
 
 class ContextFrameOccurrenceEntity(BaseEntity):
-    pass
+    """
+    The given class refers to the context frame occurrence entity which is created
+    based upon the context frame occurrence object returned from the API
+    """
+
+    def __str__(self):
+        """
+        Override the method to return the context frame occurrence id
+        """
+        return f"ContextFrameOccurrence: {self.id}"
 
 
 class BatchEntity(BaseEntity):
-    pass
+    """
+    The given class refers to the batch entity which is created based upon the batch
+    object returned from the API
+    """
+    def __str__(self):
+        """
+        Override the method to return the batch name with id
+        """
+        return f"Batch: {self.name}_{self.id}"
--- quartic_sdk/core/base_entity_list.py
+++ quartic_sdk/core/base_entity_list.py
@@ -1,8 +1,39 @@
 
+from quartic_sdk.core.base_entity import ENTITY_DICTIONARY
+
+
+class BaseEntityListIterator:
+    """
+    The given class is the entity list iterator which will be used to iterate
+    for the given list of entities
+    """
+
+    def __init__(self, base_entity_list):
+        """
+        Initialize the iterator. We set the current index as 0
+        """
+        self._base_entity_list = base_entity_list
+        self._index = 0
+
+    def __next__(self):
+        """
+        Return the subsequent iteration results starting from 0 as index
+        """
+        if len(base_entity_list.all()) > self._index:
+            self._index +=1
+            return base_entity_list[self._index]
+        raise StopIteration
+
 
 class BaseEntityList:
+    """
+    The given class refers to the list of specific `class_type` of entity
+    """
+
     def __init__(self, class_type, entities_list=[]):
         """
+        We initialize the `BaseEntityList` based upon the class_type and the
+        entities_list as passed by the user
         """
         self.class_type = class_type
         self.entities = []
@@ -11,15 +42,62 @@ class BaseEntityList:
                 self.entities.append(entity_obj)
 
     def _validate_type(self, object):
-        pass
+        """
+        We validate that the type of the object is the same as defined for the
+        class definition
+        """
+        return isinstance(object, ENTITY_DICTIONARY[self.class_type])
 
     def get(self, name, value):
+        """
+        We return the entity with the given value for the name of the attribute
+        """
         return [entity for entity in self.entities_list if entity.name == value][0]
 
+    def all(self):
+        """
+        We return the list of all entities for the given object
+        """
+        return self.entities
+
+    def count(self):
+        """
+        Returns the length of all entities
+        """
+        return len(self.entities)
+
     def filter(self, condition):
+        """
+        We filter the entities based upon the given condition
+        """
         pass
 
     def add(self, instance):
+        """
+        We add the given object instance to the entities list of the class
+        """
         if self._validate_type(instance):
             self.entities.append(instance)
 
+    def __iter__(self):
+        """
+        We override the iterator to allow the user to iterate on the class
+        """
+        return BaseEntityListIterator(self)
+
+    def __getitem__(self, key):
+        """
+        Override the method to get the entity list value at the given index
+        """
+        return self.entities[key]
+
+    def __eq__(self, other):
+        """
+        Check equality of two entity list objects
+        """
+        assert isinstance(other, BaseEntityList)
+        assert self.count() == other.count()
+        for index in self.count():
+            if not self[index] in other.all():
+                return False
+        return True
