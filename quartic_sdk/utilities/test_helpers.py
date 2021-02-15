263239429135e5d50049f9931b6cf3c488ee9a90
mprasoonquartic <mayank@quartic.ai>
2021-02-15 13:24:14 +0000

    Fix test cases

--- quartic_sdk/utilities/test_helpers.py
+++ quartic_sdk/utilities/test_helpers.py
@@ -25,4 +25,4 @@ class APIHelperCallAPI:
 ASSET_LIST_GET = [{"id": 1, "name": "Asset_name", "edge_connectors": [1, 2]}]
 TAG_LIST_GET = [{"id": 1, "name": "Tag1", "short_name": "Short Tag1", "edge_connector": 1, "asset": 1}]
 TAG_DATA_POST = {"columns": [1], "index": [1,2,3], "data": [[2], [3], [4]], "count": 1, "offset": 0}
-
+SINGLE_ASSET_GET = {"id": 2, "name": "Asset_name2", "edge_connectors": [1, 2]}
