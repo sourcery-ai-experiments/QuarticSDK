50726ac7f16aac8755f6fc8f52f529b8de412965
Shubham Dhingra <82315650+shubham-quartic@users.noreply.github.com>
2021-07-13 11:05:05 +0530

    Merge pull request #113 from Quarticai/QPD-388
    [QPD-388] Update QuarticSdk

--- quartic_sdk/_version.py
+++ quartic_sdk/_version.py
@@ -1,4 +1,8 @@
 """
 Refers to the version of the package
 """
+<<<<<<< HEAD
 __version__ = '2.1.0'
+=======
+__version__ = '2.1.dev388'
+>>>>>>> dd77444... Merge pull request #113 from Quarticai/QPD-388
