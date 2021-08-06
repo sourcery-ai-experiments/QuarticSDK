6aa2ed29dc5f5f9442c4bfd4dd408cf05218ee1e
mprasoonquartic <46556770+mprasoonquartic@users.noreply.github.com>
2021-08-06 10:56:31 +0530

    Update version (#116)

--- quartic_sdk/_version.py
+++ quartic_sdk/_version.py
@@ -1,8 +1,4 @@
 """
 Refers to the version of the package
 """
-<<<<<<< HEAD
-__version__ = '2.1.0'
-=======
-__version__ = '2.1.dev388'
->>>>>>> dd77444... Merge pull request #113 from Quarticai/QPD-388
+__version__ = 'rc1.2.2.0'
