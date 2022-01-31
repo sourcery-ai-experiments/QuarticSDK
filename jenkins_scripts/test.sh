8878e4c59bc8566eed1f45e46cc421bac7704c44
hirendra-quarticai <89519033+hirendra-quarticai@users.noreply.github.com>
2022-02-01 01:08:23 +0530

    secret update

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -1,15 +1,17 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-echo $NODE_NAME
-echo $PIPELINE_NODE
-echo $NODE_LABELS
-echo $RESERVE
-echo $PWD
-set -ex
+# echo $NODE_NAME
+# echo $PIPELINE_NODE
+# echo $NODE_LABELS
+# echo $RESERVE
+# echo $PWD
+# set -ex
 
-rand=$(shuf -i 1000000000-9999999999 -n 1)
-pip install pip==20.2.3
+# rand=$(shuf -i 1000000000-9999999999 -n 1)
+# pip install pip==20.2.3
 
-make test
-echo "build no is ${env.BUILD_NUMBER}"
-echo "end....."
+# make test
+
+# echo "end....."
+
+docker login -u $USERNAME -p $PASSWORD
