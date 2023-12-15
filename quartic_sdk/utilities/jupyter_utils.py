edcdb39ccfbad99c68c248bf3ca4e4a39ac260d4
Aman Agarwal <81435349+aman-quartic@users.noreply.github.com>
2023-12-15 16:17:43 +0530

    QPD-5887: Change how ipynb name is fetched (#317)
    * QPD-5887: Change how ipynb name is fetched
    
    * QPD-5887: Add req

--- quartic_sdk/utilities/jupyter_utils.py
+++ quartic_sdk/utilities/jupyter_utils.py
@@ -1,18 +1,4 @@
-import requests, os,subprocess, json
+import ipynbname
 
 def get_ipynb_name():
-    import ipykernel
-    connection_file = os.path.basename(ipykernel.get_connection_file())
-    kernel_id = connection_file.split('-', 1)[1].split('.')[0]
-    token = os.getenv('JPY_API_TOKEN')
-    server_url = json.loads(subprocess.check_output(['jupyter', 'server', 'list', '--json']))['url']
-    sessions_url = server_url+'api/sessions/?token='+token
-    sessions_resp = requests.get(sessions_url)
-    sessions = sessions_resp.json()
-    nb_path = None
-    for session in sessions:
-        if session['kernel']['id'] == kernel_id:
-            nb_path = session['notebook']['path']
-            break
-    return nb_path
-    
+    return ipynbname.name() + '.ipynb'
